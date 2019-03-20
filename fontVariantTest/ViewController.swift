a170a54f7c7b2d830de014452b5cbd2a63ea7d5e
cruzach <charliecruzan@gmail.com>
2019-03-20 16:56:30 -0400

    Initial Commit

---
+++ fontVariantTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fontVariantTest
+//
+//  Created by Charles Cruzan on 3/20/19.
+//  Copyright © 2019 Charles Cruzan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
